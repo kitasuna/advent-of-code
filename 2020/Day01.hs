5a86c0680cd64ba1b9fe7a8f6d167d3e3deaed86
Eric Jelliffe <onwarmermusic@gmail.com>
2020-12-25 18:17:35 +0900

    Add Day 01, Part 2 solution Update cabal file for multiple executables

--- 2020/Day01.hs
+++ 2020/Day01.hs
@@ -5,19 +5,42 @@ import qualified Data.Map as M
 tupleUp :: a -> (a, Bool)
 tupleUp = ( flip (,) ) True 
 
-f :: M.Map Integer Bool -> Integer -> Maybe Integer
-f m x = if M.member (2020 - x) m then Just (x * (2020 - x)) else Nothing
+type Target = Integer
+
+f :: M.Map Integer Bool -> Target -> Integer -> Maybe Integer
+f m t x = if M.member (t - x) m then Just (x * (t - x)) else Nothing
+
+g :: M.Map Integer Bool -> Target -> (Integer, Integer) -> Maybe Integer
+g m t pair = if M.member (t - pairSum) m then Just (pairProduct * (t - pairSum)) else Nothing
+  where pairSum = (fst pair) + (snd pair)
+        pairProduct = (fst pair) * (snd pair)
+
+pairs :: [Integer] -> [(Integer, Integer)]
+pairs [] = []
+pairs (x:[]) = []
+pairs (x:xs) = (pairs' x xs) ++ (pairs xs)
+
+pairs' :: Integer -> [Integer] -> [(Integer, Integer)]
+pairs' _ [] = []
+pairs' x (y:ys) = [(x, y)] ++ (pairs' x ys)
 
 main :: IO ()
 main = do
   input <- readFile "Day01.input"
   let numbers = fmap read (lines input) :: [Integer]
       mapped = M.fromList $ fmap tupleUp numbers 
-      maybes = fmap (f mapped) numbers
-      final = foldr (\m -> \x -> case m of 
+      maybes = fmap (f mapped 2020) numbers
+      maybes2 = fmap (g mapped 2020) (pairs numbers)
+      part1 = foldr (\m -> \x -> case m of 
                                      Just val -> val
                                      Nothing -> x
                       ) 0 maybes
+      part2 = foldr (\m -> \x -> case m of 
+                                     Just val -> val
+                                     Nothing -> x
+                      ) 0 maybes2
+
 
   print ">>> DAY 01 <<<"
-  print $ "Part 1: " ++ show final
+  print $ "Part 1: " ++ show part1
+  print $ "Part 2: " ++ show part2
